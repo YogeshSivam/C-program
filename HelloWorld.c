0309de3d0b99ea3a85e724402b7ba28d0c8b948a
YogeshSivam <yogeshsivam3@gmail.com>
2024-05-11 19:11:48 +0530

    first c program

---
+++ HelloWorld.c
@@ -0,0 +1,20 @@
+//Documentation Section
+
+/*HelloWorld program using C
+11th May 24
+YogeshSivam
+*/
+
+//Preprocessor Section
+
+#include<stdio.h> //header file("#"-Preprocessor)
+
+//main-function
+
+void main()
+{
+	printf("Hello, World!");//it is a pre-defined function which prints the string.
+}
+
+
+
