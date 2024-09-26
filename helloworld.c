2116bac9ca1ce37abb9016f9fd9c008ce878acdb
YogeshSivam <yogeshsivam3@gmail.com>
2024-09-26 11:05:45 +0530

    new changes had been made

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
+#include<stdio.h>//header files which includes standard input and output
 
-int main()
+int main()//main function where the execution starts
 {
 	printf("Helloworld!");
 }
